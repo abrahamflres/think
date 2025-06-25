79f46b18551a90c20aa22ae3de0c91c2bd1d2794
abraham <abraham@abrahams-MacBook-Air.local>
2025-06-25 16:03:51 -0400

    Edit and Update

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,29 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
   def index
   end
+
+  def show
+    @profile = current_user.profile
+  end
   def new
+  end
+
+  def edit
+    @profile = current_user.profile
+  end
+
+  def update
+    @profile = current_user.profile
+    if @profile.update(profile_params)
+      redirect_to profile_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+private
+  def profile_params
+    params.require(:profile).permit(:username, :firstname, :lastname, :bio)
   end
 end
