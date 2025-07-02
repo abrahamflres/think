8b169ded71f5f94506723d4fc59834053bd73357
abraham <abraham@abrahams-MacBook-Air.local>
2025-07-02 09:57:17 -0400

    like feature

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,18 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
+
   def create
+    article_id = params[:article_id]
+    profile_id = params[:profile_id]
+
+    article = Like.find_article_under_profile(profile_id, article_id)
+
+    if article.present?
+      Like.create_like(profile_id, article_id)
+      redirect_to profiles_path
+    else
+      redirect_to root_path
+    end
   end
 
 
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,13 @@
 class Like < ApplicationRecord
   belongs_to :profile
   belongs_to :article
+
+
+  def self.create_like(profile_id, article_id)
+    create(profile_id: profile_id, article_id: article_id)
+  end
+
+  def self.find_article_under_profile(profile_id, article_id)
+    Article.find_by(profile_id: profile_id, id: article_id)
+  end
 end
