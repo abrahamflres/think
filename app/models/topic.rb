f1637be4e313f7e7a34a4edce5d402a30d11dc6a
abraham <abraham@abrahams-MacBook-Air.local>
2025-07-15 14:54:45 -0400

    route for article topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,4 +4,9 @@ class Topic < ApplicationRecord
 
   validates :title, :description, presence: true
   validates :title, uniqueness: true
+
+
+  def self.list_topics
+    Topic.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-
+  resources :topics, only: [ :show, :index ]
 
   resources :profiles do
     resources :articles do
       resources :likes, only: [ :create ]
+      resources :article_topics, only: [ :new, :create ]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
