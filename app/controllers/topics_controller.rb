feb95f2a032ca55e3a3cc71c30ad2011d0fbfb02
abraham <abraham@abrahams-MacBook-Air.local>
2025-07-18 14:57:30 -0400

    Update, edit actions for topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,7 +5,27 @@ class TopicsController < ApplicationController
 
   def show
     topic_id = params[:id]
-
     @topic = Topic.fetch_topic(topic_id)
+  end
+  def edit
+    topic_id = params[:id]
+    @topic = Topic.fetch_topic(topic_id)
+  end
+
+  def update
+    topic_id = params[:id]
+    @topic = Topic.update_topic(topic_id, topic_params)
+    if @topic.errors.empty?
+      redirect_to topics_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  def new
+    @topic = Topic.initialize_topic
+  end
+private
+  def topic_params
+    params.require(:topic).permit(:title, :body, :image)
   end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,6 +4,8 @@ class Topic < ApplicationRecord
   validates :title, :body, presence: true
   validates :title, uniqueness: true
 
+  has_one_attached :image
+
   def self.fetch_topics
     topics = Topic.all
     topics
@@ -11,6 +13,19 @@ class Topic < ApplicationRecord
 
   def self.fetch_topic(topic_id)
     topic = Topic.find(topic_id)
+    topic
+  end
+
+
+  # TODO: form is passing nil
+  def self.update_topic(topic_id, topic_params)
+    topic = find(topic_id)
+    topic.update(topic_params)
+    topic
+  end
+
+  def self.initialize_topic
+    topic = Topic.new
     topic
   end
 end
